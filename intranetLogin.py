87817e70e2a9eac64359393c4ba6710fa405e32d
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2023-09-11 12:56:35 -0400

    Input validation

--- intranetLogin.py
+++ intranetLogin.py
@@ -4,20 +4,27 @@
 # Menu of Options: Time Reporting, Accounting, IT Helpdesk, Engineering Documents
 # Access levels: Admin (all menu items), Employee (limited access), Unauthorized (no access)
 
-# Define access levels
-    # 0 - ADMIN
-    # 1 - NO ACCESS TO JANITORIAL
-    # 2 - NO ACCESS TO ACCOUNTING 
-    # -1 - UNAUTHORIZED
-accessLvl = 0
-userInfo = []
-# Index corresponds with
-MENU_OPTIONS = ["1", "2", "3", "4", "5"]
-CHOICES = [["1", "2", "3", "4", "5"], ["2", "5"], ["3", "5"]]
+import sys
 
-## Parameters: string filename, string user, string passW
-## Returns: integer accessLvl
+# Define access levels
+MENU_OPTIONS = {"1" : "Time Reporting",
+                "2" : "Accounting",
+                "3" : "Engineering Documents",
+                "4" : "HR Documents",
+                "5" : "IT Helpdesk",
+                "6" : "User Management",
+                "7" : "Security Settings",
+                "8" : "Exit"}
+MENU_ACCESS = [["1", "2", "3", "4", "5", "6", "7", "8"], ["1", "2", "3", "5", "8"], ["1", "4", "5", "8"]]
+USER_DATA = "intranetInfo.csv"
+accessLvl = 0
+
+## Authenticate function takes in a filename, username, and password,
+## then searches the file for the username and password. The function
+## returns an access level 0-2 if the username and password match one
+## in the file. Returns -1 if the information is not found.
 def authenticate(filename, user, passW):
+    userInfo = []
     try:
         with open(filename, "r") as inFile:
             junk = inFile.readline()
@@ -26,76 +33,90 @@ def authenticate(filename, user, passW):
                 if userInfo[0] == user and userInfo[1] == passW:
                     return int(userInfo[2])
     except FileNotFoundError:
-        print("Error:", filename, "not found.")
+        print("Error:", filename, "not found. Please review the file and try again.")
+        sys.exit()
     return -1
 
-## Returns: integer accessLvl
-def login():
-    filename = "intranetInfo.csv"
-    
-    print("Intranet Login\n<><><><><><><><><><>")
-    
-    user = input("Username: ")
-    passW = input("Password: ")
 
-    accessLvl = authenticate(filename, user, passW)
-    while accessLvl == -1:
-        print("Username or password is incorrect. Please try again.")
+## Login function prompts the user to enter their username and password,
+## then calls the authenticate function to verify the information.
+## Continues to prompt the user if incorrect information is given.
+def login():
+    valid = False;
+    print("Intranet Login\n<><><><><><><><><><>")
+
+    while valid == False:
         user = input("Username: ")
+        while user == "":
+            valid == False
+            print("No input detected. Please try again.")
+            user = input("Username: ")
+            
         passW = input("Password: ")
-        accessLvl = authenticate(filename, user, passW)
+        while passW == "":
+            valid = False
+            print("No input detected. Please try again.")
+            passW = input("Password: ")
+            
+        accessLvl = authenticate(USER_DATA, user, passW)
+        if accessLvl == -1:
+            valid = False
+            print("Username or password is incorrect. Please try again.")
+            #accessLvl = authenticate(USER_DATA, user, passW)
+        else:
+            
+            valid = True
         
     return accessLvl
-        
+
+## Print_menu function prints the MENU_OPTIONS dict
 def print_menu():
     print("+--------MENU--------+")
-    print("| 1 - Admin          |")
-    print("| 2 - Accounting     |")
-    print("| 3 - Janitorial     |") 
-    print("| 4 - Schedule Edit  |")
-    print("| 5 - Exit           |")
+    for key, value in MENU_OPTIONS.items():
+        print(" ", key, "-", value)
     print("+--------------------+")
 
+## Back_to_menu takes in an access level, then prompts the user
+## To return to the main menu
 def back_to_menu(accessLvl):
     back = ""
     back = input("Press enter to go back to the menu: ")
     menu_access(accessLvl)
-        
+
+## Menu access takes in an access level and prompts the user to
+## choose an option. Based on the access level, the function grants
+## or denies access to the chosen menu item.
 def menu_access(accessLvl):
     print_menu()
     choice = input("Choose a menu option: ")
 
     # Validate that input is a choice in menu
-    while choice not in MENU_OPTIONS:
+    while choice not in MENU_ACCESS[0]:
         print("That choice is not in the menu!")
         choice = input("Choose a menu option: ")
 
     # Go to area or deny access
-    if choice in CHOICES[accessLvl]:
-        if choice == "1":
-            print("You have entered the admin area")
-            back_to_menu(accessLvl)
-        elif choice == "2":
-            print("You have entered the accounting area")
-            back_to_menu(accessLvl)
-        elif choice == "3":
-            print("You have entered the janitorial area")
-            back_to_menu(accessLvl)
-        elif choice == "4":
-            print("You have entered the schedule area")
-            back_to_menu(accessLvl)
-        else:
-            print("See you soon!")
-            return
+    if choice in MENU_ACCESS[accessLvl] and choice != "8":
+        print("You have now accessed the", MENU_OPTIONS[choice], "area.")
+        back_to_menu(accessLvl)
+    elif choice == "8":
+        print("See you soon!")
+        return
     else:
         print("XX You do not have access to this menu area! XX")
         back_to_menu(accessLvl)
         
 
 ##########
-        
+
 accessLvl = login()
+
 menu_access(accessLvl)
+
+
+
+
+#### TESTING
 
     
 
