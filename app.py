f43810f08fecd5c4b16ddb23de6a285e22150fee
hannah <hgsherid@uvm.edu>
2023-11-23 14:30:36 -0500

    Creating login functionality

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
+from intranetLogin import authenticate
 
 app = Flask(__name__, static_folder="instance/static")
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        accessLvl = authenticate("intranetInfo.csv", user, password)
+        if accessLvl != -1:
+            return "Hey, " + user + ", welcome! Your access level is " + str(accessLvl)
+        else:
+            return render_template('home.html')
+
     return render_template('home.html')
 
 
-@app.errorhandler(404)
-def page_not_found():
-    return render_template('404.html')
